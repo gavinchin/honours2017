a60bacb4c8839fd09e81b8ba89f69e696fee4126
gavinchin <gcchi4@student.monash.edu>
2017-05-26 14:42:04 +1000

    test code for matching public holidays to generate var "HDay"

--- code/work.R
+++ code/work.R
@@ -34,10 +34,13 @@ pub_hday16 <- read_csv("http://data.gov.au/dataset/b1bc6077-dadd-4f61-9f8c-002ab
   pub_hdays$VIC[grep("NAT", pub_hdays$`Applicable To`)] <- 1
 
   ped_df$HDay <- 0
-for (i in 1:nrow(ped_df)){
-  if (ped_df$Date_Time[i] %in% pub_hdays$Date[pub_hdays$VIC == 1]) {
-      ped_df$HDay[i] <- "Holiday"
+  
+  ped_testdf <- filter(ped_df, Sensor_ID == 1)
+for (i in 1:nrow(ped_testdf)){
+  if (ped_testdf$Date_Time[i] %in% pub_hdays$Date[pub_hdays$VIC == 1]) {
+      ped_testdf$HDay[i] <- "Holiday"
   }
-  else {ped_df$HDay[i] <- ped_df$Day[i]}
+  else {ped_testdf$HDay[i] <- ped_testdf$Day[i]}
 }
+  
   
