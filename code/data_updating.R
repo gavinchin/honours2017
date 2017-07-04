180777a75ff18a0f8c015d8b4783a0ae08d7e201
gavinchin <gcchi4@student.monash.edu>
2017-07-04 17:42:20 +0800

    reformats last available date in correct format for url. creating loop for downloading each missing day

--- code/data_updating.R
+++ code/data_updating.R
@@ -5,8 +5,22 @@ library(readr)
 library(dplyr)
 ped_df <- read_csv("data/Pedestrian_volume__updated_monthly_.csv")
 
-  last_date <- sr.strsplit(tail(ped_df$Date_Time, 1), " ", drop = TRUE)
+  last_date <- parse_date(tail(ped_df$Date_Time, 1), "%d-%b-%Y %H:%M")
   last_today_date <- as.Date(today()-1, "%d-%m-%y")
-  last_date_day <- mday(today())
-
-##x <- read_csv(sprintf("https://compedapi.herokuapp.com/api/bydatecsv/%s?", last_today_date), skip=8)
+  for (i in seq(last_date, last_today_date, by = "day"))
+    if (mday(i) < 10) {
+      i_date <- paste(0, mday(i), sep = "")
+    }
+    else {
+      i_date <- mday(i)
+    }
+    if (month(i) < 10) {
+      i_month <- paste(0, month(i), sep = "")
+    }
+    else {
+      i_month <- month(i)
+    }
+    i_year <- year(i)
+    last_day_date <- paste(i_date, i_month, i_year, sep ="-")
+  
+x <- read_csv(sprintf("https://compedapi.herokuapp.com/api/bydatecsv/%s?", last_today_date), skip=8)
