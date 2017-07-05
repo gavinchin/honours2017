1881a6d4c373e1cf83bb8279693063923d8f256b
gavinchin <gcchi4@student.monash.edu>
2017-07-05 09:31:32 +0800

    updated code - forced i_date and i_month as strings to ensure paste() works as intended

--- code/data_updating.R
+++ code/data_updating.R
@@ -7,20 +7,22 @@ ped_df <- read_csv("data/Pedestrian_volume__updated_monthly_.csv")
 
   last_date <- parse_date(tail(ped_df$Date_Time, 1), "%d-%b-%Y %H:%M")
   last_today_date <- as.Date(today()-1, "%d-%m-%y")
-  for (i in seq(last_date, last_today_date, by = "day"))
+x <- 0  
+  for (i in as.list(seq(last_date, last_today_date, by = "day"))){
     if (mday(i) < 10) {
       i_date <- paste(0, mday(i), sep = "")
     }
-    else {
-      i_date <- mday(i)
-    }
+      else {
+      i_date <- as.character(mday(i))
+      }
     if (month(i) < 10) {
       i_month <- paste(0, month(i), sep = "")
-    }
-    else {
-      i_month <- month(i)
-    }
+                        }
+      else {
+      i_month <- as.character(month(i))
+      }
     i_year <- year(i)
     last_day_date <- paste(i_date, i_month, i_year, sep ="-")
-  
-x <- read_csv(sprintf("https://compedapi.herokuapp.com/api/bydatecsv/%s?", last_today_date), skip=8)
+    url_to_scrape <- sprintf("https://compedapi.herokuapp.com/api/bydatecsv/%s?", last_day_date)
+
+  }
