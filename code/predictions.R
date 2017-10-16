919ce4d27e992eaa20c78d0d6432696b327b9a98
unknown <gcchi4@student.monash.edu>
2017-10-16 18:38:10 +1100

    need to improve efficiency - 3.3 seconds for full day predictions, 130ms for single hour prediction. need to get roxygen working

--- code/predictions.R
+++ code/predictions.R
@@ -2,6 +2,7 @@ library(purrr)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(roxygen2)
 load("data/model_est.RData")
 
 daytype <- function(date){
@@ -14,13 +15,25 @@ daytype <- function(date){
                           "Midweek"))))
   return(x_daytype)
 }
-
+#' Predicted pedestrian counts at various sensor locations in Melbourne
+#'
+#' @param pred_date input date
+#' @param t_hour input integer
+#' @param is_pub_hol input logical
+#' @return data frame of the fitted values at each location in wide format, with time and date
+#' @export
+#' @examples
+#' ped_predict()
+#' ped_predict("2017-12-25", 18, TRUE)
 ped_predict <- function(pred_date = today(tzone = "Australia/Melbourne"),
                         t_hour = 12,
                         is_pub_hol = FALSE) {
   if(!(t_hour %in% seq(0, 23))){
     stop("Hour is not between 0:00 and 23:00. Please give hour value between 0 and 23")}
-  if(!is.Date(pred_date)){stop("Date given to predict is not a date. Use lubridate::ymd()")}
+  pred_date <- ymd(pred_date)
+  if(!is.Date(pred_date)){
+    stop(paste("Date given to predict cannot be parsed as a date. Use the date format 'yyyy-mm-dd'. Example: 25 January 2018 is '2018-01-25'."))
+    }
  
   date_pred <- data.frame(Time = as.factor(t_hour)) %>%
                mutate(DayType = daytype(pred_date),
@@ -32,7 +45,17 @@ ped_predict <- function(pred_date = today(tzone = "Australia/Melbourne"),
     preds[i] <- predict(pred_model[[i]], newdata = date_pred, type = "response")
   }
   preds_df <- data.frame("fit" = preds, "Sensor_ID" = names(preds)) %>%
-    spread(Sensor_ID, fit) %>% cbind(date_pred)
+    spread(Sensor_ID, fit) %>% cbind(date_pred) %>% as_tibble
   
   return(preds_df)
+}
+
+ped_predict_day <- function(pred_date = today(tzone = "Australia/Melbourne"),
+                            is_pub_hol = FALSE){
+  day_df <- data.frame()
+      for(t in 0:23){
+        t_preds <- ped_predict(pred_date, t_hour = t, is_pub_hol)
+        suppressWarnings(day_df <- bind_rows(day_df, t_preds))
+      }
+  return(as.tibble(day_df))
 }
